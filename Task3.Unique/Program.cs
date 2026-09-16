dd61f4810f68c9f0baa3e397e021efebb64c6b7c
Lavrov Yaroslav <pdiddyye67@gmail.com>
2026-09-16 13:42:55 +0300

    Task3: уникальные элементы без LINQ

--- Task3.Unique/Program.cs
+++ Task3.Unique/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task3.Unique
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] source = { 1, 2, 2, 3, 4, 4, 4, 5 };
+            Console.WriteLine($"Исходный:  {string.Join(", ", source)}");
+
+            int[] unique = GetUnique(source);
+            Console.WriteLine($"Уникальные: {string.Join(", ", unique)}");
+        }
+
+        public static int[] GetUnique(int[] source)
+        {
+            if (source.Length == 0) return new int[0];
+            int[] tempResult = new int[source.Length];
+            int uniqueCount = 0;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                bool alreadyExists = false;
+
+                for (int j = 0; j < uniqueCount; j++)
+                {
+                    if (source[i] == tempResult[j])
+                    {
+                        alreadyExists = true;
+                        break;
+                    }
+                }
+
+                if (!alreadyExists)
+                {
+                    tempResult[uniqueCount] = source[i];
+                    uniqueCount++;
+                }
+            }
+
+            int[] finalResult = new int[uniqueCount];
+            for (int i = 0; i < uniqueCount; i++)
+            {
+                finalResult[i] = tempResult[i];
+            }
+
+            return finalResult;
+        }
+    }
+}
